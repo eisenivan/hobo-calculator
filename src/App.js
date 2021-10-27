a421457004de0e8ab2389bb08a3872647f8595a0
Brian Welter <brianwelter@northwesternmutual.com>
2021-10-27 14:46:46 -0500

    modernize the hobo calculator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import regent from 'regent'
 import './App.css'
 import {
@@ -20,30 +20,26 @@ import Moustache from './Moustache'
 
 const { rule } = regent.init()
 
-class App extends Component {
-  constructor (props) {
-    super()
-    this.state = {
-      income: 50000,
-      monthlyCannedBeanConsumption: 3,
-      preferredClothesContainer: 'suitcase',
-      fireOptimalHeatingLocation: 'furnace',
-      fireOptimalCookingLocation: 'stove',
-      toesVisibleThroughShoes: 'yes',
-      likelihoodOfWindowsillPieTheft: 2,
-      preferredTrainCarType: 'passenger',
-      steelMillsOwned: 0,
-      carsOwned: 0
-    }
-
-    this.handleChange = this.handleChange.bind(this)
+const App = () => {
+  const [state, setState] = useState({      
+    monthlyCannedBeanConsumption: 3,
+    preferredClothesContainer: 'suitcase',
+    fireOptimalHeatingLocation: 'furnace',
+    fireOptimalCookingLocation: 'stove',
+    toesVisibleThroughShoes: 'yes',
+    likelihoodOfWindowsillPieTheft: 2,
+    preferredTrainCarType: 'passenger',
+    steelMillsOwned: 0,
+    carsOwned: 0
+  })
+  
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
   }
 
-  handleChange (event) {
-    this.setState({[event.target.name]: event.target.value})
-  }
-
-  render () {
     return (
       <div className='App'>
         <header className='App-header'>
@@ -53,84 +49,83 @@ class App extends Component {
         <form className='input-form pure-form pure-form-stacked'>
           <label>What is your yearly income?</label>
           <div className='input-container'>
-            <input type='text' name='income' value={this.state.income} onChange={this.handleChange} />
-            { rule(this.state, hoboIncome) ? Bindle : null }
-            { rule(this.state, tycoonIncome) ? Moustache : null }
+            <input type='text' name='income' value={state.income} onChange={handleChange} />
+            { rule(state, hoboIncome) ? Bindle : null }
+            { rule(state, tycoonIncome) ? Moustache : null }
           </div>
 
           <label>How many cars do you currently own?</label>
           <div className='input-container'>
-            <input type='number' name='carsOwned' value={this.state.carsOwned} onChange={this.handleChange} />
-            { rule(this.state, notMotorist) ? Bindle : null }
-            { rule(this.state, motoristTycoon) ? Moustache : null }
+            <input type='number' name='carsOwned' value={state.carsOwned} onChange={handleChange} />
+            { rule(state, notMotorist) ? Bindle : null }
+            { rule(state, motoristTycoon) ? Moustache : null }
           </div>
 
           <label>How many steel mills do you currently own?</label>
           <div className='input-container'>
-            <input type='number' name='steelMillsOwned' value={this.state.steelMillsOwned} onChange={this.handleChange} />
-            { rule(this.state, notSteelTycoon) ? Bindle : null }
-            { rule(this.state, steelTycoon) ? Moustache : null }
+            <input type='number' name='steelMillsOwned' value={state.steelMillsOwned} onChange={handleChange} />
+            { rule(state, notSteelTycoon) ? Bindle : null }
+            { rule(state, steelTycoon) ? Moustache : null }
           </div>
 
           <label>How many cans of beans would you say you eat per month?</label>
           <div className='input-container'>
-            <input type='number' name='monthlyCannedBeanConsumption' value={this.state.monthlyCannedBeanConsumption} onChange={this.handleChange} />
-            { rule(this.state, hoboBeanConsumption) ? Bindle : null }
+            <input type='number' name='monthlyCannedBeanConsumption' value={state.monthlyCannedBeanConsumption} onChange={handleChange} />
+            { rule(state, hoboBeanConsumption) ? Bindle : null }
           </div>
 
           <label>How would you prefer to pack your clothes for a trip?</label>
           <div className='input-container'>
-            <select value={this.state.preferredClothesContainer} name='preferredClothesContainer' onChange={this.handleChange}>
+            <select value={state.preferredClothesContainer} name='preferredClothesContainer' onChange={handleChange}>
               <option value='suitcase'>Suitcase</option>
               <option value='bindle'>Bindle</option>
             </select>
-            { rule(this.state, hoboPackingStyle) ? Bindle : null }
+            { rule(state, hoboPackingStyle) ? Bindle : null }
           </div>
 
           <label>What is the ideal place for a heating fire?</label>
           <div className='input-container'>
-            <select value={this.state.fireOptimalHeatingLocation} onChange={this.handleChange} name='fireOptimalHeatingLocation'>
+            <select value={state.fireOptimalHeatingLocation} onChange={handleChange} name='fireOptimalHeatingLocation'>
               <option value='furnace'>Furnace</option>
               <option value='fireplace'>Fireplace</option>
               <option value='rusty-barrel'>Rusty barrel</option>
             </select>
-            { rule(this.state, hoboHeatingFire) ? Bindle : null }
+            { rule(state, hoboHeatingFire) ? Bindle : null }
           </div>
 
           <label>Where would you rather light a cooking fire?</label>
           <div className='input-container'>
-            <select value={this.state.fireOptimalCookingLocation} onChange={this.handleChange} name='fireOptimalCookingLocation'>
+            <select value={state.fireOptimalCookingLocation} onChange={handleChange} name='fireOptimalCookingLocation'>
               <option value='stove'>Stove</option>
               <option value='grill'>Grill</option>
               <option value='rusty-barrel'>Rusty barrel</option>
             </select>
-            { rule(this.state, hoboCookingFire) ? Bindle : null }
+            { rule(state, hoboCookingFire) ? Bindle : null }
           </div>
 
           <label>With your shoes properly on, can you see your toes?</label>
           <div className='input-container'>
-            <select value={this.state.toesVisibleThroughShoes} onChange={this.handleChange} name='toesVisibleThroughShoes'>
+            <select value={state.toesVisibleThroughShoes} onChange={handleChange} name='toesVisibleThroughShoes'>
               <option value='yes'>Yes</option>
               <option value='false'>No</option>
             </select>
-            { rule(this.state, hoboFootwear) ? Bindle : null }
+            { rule(state, hoboFootwear) ? Bindle : null }
           </div>
 
           <label>How likely are you to steal a pie that was left on a windowsill to cool?</label>
           <div className='input-container'>
-            <select value={this.state.likelihoodOfWindowsillPieTheft} onChange={this.handleChange} name='likelihoodOfWindowsillPieTheft'>
+            <select value={state.likelihoodOfWindowsillPieTheft} onChange={handleChange} name='likelihoodOfWindowsillPieTheft'>
               <option value='0'>I would never do that</option>
               <option value='1'>I probably wouldn't do that</option>
               <option value='2'>What kind of pie?</option>
               <option value='3'>Yeah I'd probably take that pie</option>
               <option value='4'>100% for sure stealing that pie</option>
             </select>
-            { rule(this.state, hoboPieHabits) ? Bindle : null }
+            { rule(state, hoboPieHabits) ? Bindle : null }
           </div>
         </form>
       </div>
     )
   }
-}
 
 export default App
